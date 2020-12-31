5a7608915f133bc84b534c57d95be2100fdc498a
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-31 16:10:28 -0300

    feat: create contact controller and initialize method

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index(request, response) {
+    response.send('Contact Controller Index Method');
+  }
+
+  show() {
+
+  }
+
+  store() {
+
+  }
+
+  update() {
+
+  }
+
+  delete() {
+
+  }
+}
+
+module.exports = new ContactController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 
+const ContactController = require('./app/controllers/ContactController');
+
 const router = Router();
 
-router.get('/', (request, response) => {
-  response.send('Hello World');
-});
+router.get('/contacts', ContactController.index);
 
 module.exports = router;
