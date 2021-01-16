32d074fbd8209c6846850367825db2b673d1fb2c
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-16 20:08:53 -0300

    feat: change the server constant by app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const routes = require('./routes');
 
-const server = express();
-server.use(express.json());
-server.use(routes);
+const app = express();
+app.use(express.json());
+app.use(routes);
 
-server.listen(3001, () => {
+app.listen(3001, () => {
   console.log('🔥 server started at http://localhost:3001');
 });
