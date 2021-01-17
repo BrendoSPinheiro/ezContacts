70047c648660c269e321d7fbfe6caffabbdd0cdb
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-17 15:16:49 -0300

    feat: add show contact

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -7,8 +7,16 @@ class ContactController {
     response.json(contacts);
   }
 
-  show() {
+  async show(request, response) {
+    const { id } = request.params;
 
+    const contact = await ContactRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    response.json(contact);
   }
 
   store() {
--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -21,6 +21,12 @@ class ContactRepository {
   findAll() {
     return new Promise((resolve) => resolve(contacts));
   }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
 }
 
 module.exports = new ContactRepository();
