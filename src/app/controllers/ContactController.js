0211e865b5c2342c56fc45cf65adcf0bf5236fb8
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-22 03:10:39 -0300

    feat: add create and update contact and findByEmail, create and update in contact repository

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -19,12 +19,49 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
+    const { name, email, phone, category_id } = request.body;
 
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This email already in use' });
+    }
+
+    const contact = await ContactRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
+    const { id } = request.params;
+    const { name, email, phone, category_id } = request.body;
 
+    const contactExists = await ContactRepository.findById(id);
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactRepository.findByEmail(email);
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This email already in use' });
+    }
+
+    const contact = await ContactRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
