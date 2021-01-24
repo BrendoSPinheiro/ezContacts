4f04aba8d4999adebe8b7d1e91d21aa6c0cde225
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-24 18:45:57 -0300

    feat: create connection with database

---
+++ src/database/index.js
@@ -0,0 +1,18 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  // dados de conexão à base de dados
+  host: 'localhost',
+  port: 5432,
+  // dados de acesso à base de dados
+  user: 'root',
+  password: 'root',
+  database: 'ezcontacts',
+});
+
+client.connect();
+
+exports.query = async (query) => {
+  const { rows } = await client.query(query);
+  return rows;
+};
