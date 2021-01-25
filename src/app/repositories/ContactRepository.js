a9429247a93fa9de55f09ec7bfbbc8db5dcd7b73
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-25 01:01:17 -0300

    refactor: refactor delete method in contact repository and remove uuid

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Brendo Souza',
-    email: 'brendo@mail.com',
-    phone: '71999999999',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Maisa Greice',
-    email: 'maisa@mail.com',
-    phone: '71999999999',
-    category_id: v4(),
-  },
-];
 
 class ContactRepository {
   async findAll(orderBy = 'ASC') {
@@ -65,12 +46,13 @@ class ContactRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
+  async delete(id) {
+    const deleteOp = await db.query(`
+      DELETE FROM contacts
+      WHERE id = $1
+    `, [id]);
 
-      resolve();
-    });
+    return deleteOp;
   }
 }
 
