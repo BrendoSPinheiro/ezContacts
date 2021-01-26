5ca2f62eb7179d987cac1f7a73f3cb81a444e1f8
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-25 21:28:03 -0300

    feat: add create category route

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -6,6 +6,18 @@ class CategoryController {
 
     response.json(categories);
   }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoryRepository.create({ name });
+
+    response.json(category);
+  }
 }
 
 module.exports = new CategoryController();
