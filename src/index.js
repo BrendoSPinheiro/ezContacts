39760e68cdcd1c3f29f305de064dcc860600dfd4
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-28 11:19:20 -0300

    feat: error handler

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const routes = require('./routes');
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('########');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3001, () => {
   console.log('🔥 server started at http://localhost:3001');
